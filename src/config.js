f7c614b030af2c2c9d5401a5f93dedee1d8ebce9
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-08-31 14:33:08 -0700

    mosre stuff

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
-}
+  API_ENDPOINT: "http://localhost:8000/api",
+  // API_ENDPOINT: 'https://morning-scrubland-26451.herokuapp.com/api',
+  TOKEN_KEY: "blogful-client-auth-token",
+};
